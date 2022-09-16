514bc74ad1a2e5a6462e7eaa1e36482f03830a1a
Kochetkov Konstantin <kmkochetkov@gmail.com>
2022-09-16 14:22:42 +0300

    ДЗ 3, задача 19

---
+++ hw_c_003_task_019/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+bool PalindromChecker(string num)
+{
+    for (int i = 0; i < num.Length / 2; ++i)
+        if (num[i] != num[num.Length - 1 - i])
+        return false;
+        return true;
+}
+
+
+Console.WriteLine("Введите число для проверки на палиндром: ");
+
+int num = 0;
+try
+    {
+        num = Convert.ToInt32(Console.ReadLine());
+    }
+catch
+    {    
+        Console.WriteLine("Нужно вводить целое число!");
+    }
+
+
+
+// По условию задачи число пятизначное. По этой причине вводится проверка на соответствие целому
+
+// Проверка числа на палиндром происходит через преобразование в строку.
+// Данный метод позволяет проверять строки из символов.
+
+
+string number = Convert.ToString(num);
+
+if (PalindromChecker(number)==false)
+    {
+        Console.WriteLine("Введённое число не является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Введённое число палиндром");
+    }
